e172c150a0e88a675ff3f4e5eb57ca5cdbfb4dc1
Luis Smith <luis.gourde@gmail.com>
2023-10-16 23:35:03 +0200

    first

--- scripts/decoder.py
+++ scripts/decoder.py
@@ -1,15 +1,9 @@
 import struct
 
-# The binary data in little-endian format
-with open("datas/dataset.txt", "r") as file:
-    hex_data = file.readlines()
+# Initialize an index to keep track of where we are in the data
+index = 0
 
-# Combine the lines into a single hexadecimal string
-hex_data = "".join(hex_data).strip()
-
-# Convert the hexadecimal string to bytes
-data = bytes.fromhex(hex_data)
-# Define the format for each field (little-endian)
+# Define the field format
 field_format = "<i"  # Use "i" for signed integers and "f" for floating-point numbers
 
 # Define the field names and their corresponding format
@@ -106,13 +100,27 @@ field_definitions = [
     ("TrackOrdinal", "i")
 ]
 
-# Initialize an index to keep track of where we are in the data
-index = 0
+# Open the file and read the lines
+with open("datas/dataset.txt", "r") as file:
+    lines = file.readlines()
 
-# Unpack and print each field
-for field_name, field_format in field_definitions:
-    field_size = struct.calcsize(field_format)
-    field_data = data[index:index + field_size]
-    field_value = struct.unpack(field_format, field_data)[0]
-    print(f"{field_name}: {field_value}")
-    index += field_size
+# Iterate through the lines and process each one
+for line in lines:
+    # Remove any leading/trailing whitespace and convert to a hexadecimal string
+    hex_data = line.strip()
+
+    # Convert the hexadecimal string to bytes
+    data = bytes.fromhex(hex_data)
+
+    # Reset the index for each line
+    index = 0
+
+    # Unpack and print each field
+    for field_name, field_format in field_definitions:
+        field_size = struct.calcsize(field_format)
+        field_data = data[index:index + field_size]
+        field_value = struct.unpack(field_format, field_data)[0]
+        print(f"{field_name}: {field_value}")
+        index += field_size
+
+    print()  # Add a newline between lines
