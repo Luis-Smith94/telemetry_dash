2e08ba76f8072112a6a4a1aa7614c3094767b268
Luis GOURDE <luis.gourde@gmail.com>
2023-10-17 18:00:30 +0200

    fix sender/receiver

--- core/udp_receiver.py
+++ core/udp_receiver.py
@@ -1,6 +1,6 @@
 import socket
 
-HOST_IP = "10.4.124.12"
+HOST_IP = "0.0.0.0"
 HOST_PORT = 5300
 BUFFER_SIZE = 1024
 
@@ -8,6 +8,8 @@ s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.bind((HOST_IP, HOST_PORT))
 
 print(f"Attente de message sur {HOST_IP}, port {HOST_PORT}...")
-data, addr = s.recvfrom(BUFFER_SIZE)
-print(data)
+while True:
+    data, addr = s.recvfrom(BUFFER_SIZE)
+    print(data.hex())
+# print(data)
 
