4be45504169cbff0e744ed66511e5558cfef2a5c
Valentin PERARD <v.perard@hotmail.fr>
2023-10-18 23:34:22 +0200

    style: removes commented code

--- core/test_dash_decoder.py
+++ core/test_dash_decoder.py
@@ -2,18 +2,12 @@ import dash_decoder as decoder
 
 def readLine(path: str) -> bytes:
     # The binary data in little-endian format
-    #data = []
-    #print("==================================================")
     with open(path, "r") as file:
         hex_data = file.readline()
         # Combine the lines into a single hexadecimal string
         hex_data = "".join(hex_data).strip()
         # Convert the hexadecimal string to bytes and add it to the array
         return bytes.fromhex(hex_data)
-        #print(data)
-        #print("-----------------------------------------------------------")
-    #print("-----------------------------------------------------------")
-    #return data
 
 def readFile(path: str) -> bytes:
     full_data = []
