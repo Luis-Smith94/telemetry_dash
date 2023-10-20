22b12eda9a15cba515e55d53fff559562e959af6
Valentin PERARD <v.perard@hotmail.fr>
2023-10-20 22:15:04 +0200

    feat: adds accel chart

--- maybe_main/test_main.py
+++ maybe_main/test_main.py
@@ -12,9 +12,11 @@ import numpy as np
 
 # Create figure for plotting
 fig = plt.figure()
-ax = fig.add_subplot(1, 1, 1)
+ax = fig.add_subplot(211)
+bx = fig.add_subplot(212)
 xs = []
-ys = []
+yas = []
+ybs = []
 
 def bus():
     HOST_IP = "127.0.0.1"
@@ -26,7 +28,7 @@ def bus():
 
 
 # This function is called periodically from FuncAnimation
-def animate(i, xs, ys):
+def animate(i, xs, yas, ybs):
 
     HOST_IP = "127.0.0.1"
     HOST_PORT = 5300
@@ -34,27 +36,32 @@ def animate(i, xs, ys):
     decoded_data = decoder.decode_packet(data)
     # Read temperature (Celsius) from TMP102
     brake = decoded_data.get("Brake", -1) / 255*100
-    TimestampMS = decoded_data.get("TimestampMS", -1)
+    accel = decoded_data.get("Accel", -1) / 255*100
+    TimestampMS = decoded_data.get("CurrentRaceTime", -1)
 
     # Add x and y to lists
     xs.append(TimestampMS)
-    ys.append(brake)
+    yas.append(brake)
+    ybs.append(accel)
 
     # Limit x and y lists to 20 items
-    xs = xs[-20:]
-    ys = ys[-20:]
+    xs = xs[-200:]
+    yas = yas[-200:]
+    ybs = ybs[-200:]
 
     # Draw x and y lists
     ax.clear()
-    ax.plot(xs, ys)
+    ax.plot(xs, yas, label='Accel', color='red')
+    bx.clear()
+    bx.plot(xs, ybs, label='Accel', color='green')
 
     # Format plot
     plt.xticks(rotation=45, ha='right')
     plt.subplots_adjust(bottom=0.30)
-    plt.title('TMP102 Temperature over Time')
-    plt.ylabel('Temperature (deg C)')
-    plt.ylim(0,100)
+    # plt.title('Breaking pressure over Time')
+    # plt.ylabel('Breaking pressure (%)')
+    plt.ylim(-5,105)
 
 # Set up plot to call animate() function periodically
-ani = animation.FuncAnimation(fig, animate, fargs=(xs, ys), interval=0.0001)
+ani = animation.FuncAnimation(fig, animate, fargs=(xs, yas, ybs), interval=0.0001)
 plt.show()
